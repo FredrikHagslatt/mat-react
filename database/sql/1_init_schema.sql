c9d7c63f5661cc533da5d7b3cbe7aa7540b26144
Fredrik Hagslätt <fredrik.hagslatt@gmail.com>
2023-07-10 13:51:40 +0200

    Added description_type to database

--- database/sql/1_init_schema.sql
+++ database/sql/1_init_schema.sql
@@ -3,10 +3,11 @@ CREATE TABLE recipes (
     created_at          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at          timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     name                varchar(64) NOT NULL UNIQUE,
-    url                 varchar(300),
     image               varchar(300) NOT NULL DEFAULT 'no_image_added.jpg',
     queue_pos           int NOT NULL DEFAULT 99,
+    description_type    varchar(16) NOT NULL DEFAULT 'internal',
     description         varchar(1024)
+    url                 varchar(300),
 );
 
 
@@ -46,6 +47,3 @@ ON ingredients FOR EACH ROW EXECUTE PROCEDURE update_updated_at();
 
 CREATE TRIGGER update_association_table_timestamp BEFORE UPDATE
 ON association_table FOR EACH ROW EXECUTE PROCEDURE update_updated_at();
-
-
-
--- database/sql/2_sample_info.sql
+++ database/sql/2_sample_info.sql
@@ -32,50 +32,53 @@ INSERT INTO public.ingredients (name) VALUES ('Dijonsenap');
 
 -- Data for Name: recipes; Type: TABLE DATA; Schema: public; Owner: freppan
 
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (43, 'Tortellinis med röd pestosås', '', 44, 'Koka tortellinis.
+
+INSERT INTO public.recipes (id, name, url, image, queue_pos, description_type) VALUES (31, 'Biff Stroganoff', 'https://www.ica.se/recept/biff-stroganoff-713113/', 'biff_stroganoff.jpg', 14, 'external');
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (29, 'Paneer masala', 'https://www.arla.se/recept/paneer-masala/', 39, 'external');
+INSERT INTO public.recipes (id, name, queue_pos) VALUES (19, 'Pestopasta', 20);
+INSERT INTO public.recipes (id, name, queue_pos) VALUES (33, 'Köttbullar med mos', 19);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (34, 'Potatis- och purjulöksoppa', 'https://www.ica.se/recept/kramig-potatis-och-purjolokssoppa-716077/?fbclid=IwAR1iAeq9i_hTThumFcRYaDEQAlabl7McopIaJaC6qy3b-3BQGSTe8k0Jl1s', 8);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (38, 'Abbalax', 'https://www.abba.se/products/citronsas/', 24);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (22, 'Tomatsoppa', 'https://www.koket.se/anders_leven/soppor_och_grytor/gronsaker/tomatsoppa_med_basilikatoast/?fbclid=IwAR2pWyTsjepaonxuk0UIfIugu5YBtX-dN2BA7gYeTw1yLnZyFQ7b7hWcFsI', 64);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (17, 'Kräftstjärtspasta', 'https://www.arla.se/recept/morotspasta-med-kraftor/', 51);
+INSERT INTO public.recipes (id, name, queue_pos, description_type) VALUES (23, 'Tacopaj', 49);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (27, 'Enchiladas', 'https://www.santamariaworld.com/se/recept/enchiladas-med-kyckling/?fbclid=IwAR0xpPIemDLKOkArF5kHfIfT5aZYP53OgWB_UaAM5FPPBCljmAQ2gAsoYTA', 35);
+INSERT INTO public.recipes (id, name, queue_pos, description_type) VALUES (39, 'Fiskpinnar med stuvad spenat', 16);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (20, 'Krämig kycklinggryta med bacon och soltorkade tomater', 'https://www.tasteline.com/recept/kramig-kycklinggryta-med-kycklingbacon-och-soltorkade-tomater/', 13);
+INSERT INTO public.recipes (id, name, queue_pos, description_type) VALUES (21, 'Thaisoppa', 11);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (28, 'Asiatisk kokosgryta med lax', 'https://www.tasteline.com/recept/asiatisk-kokosgryta-med-lax/', 7);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (13, 'Busenkel korvröra', 'http://matochmera.se/recepie.php?recepie_id=31348', 104);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (12, 'Fredrik hittar på något nytt', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 98);
+INSERT INTO public.recipes (id, name, url, image, queue_pos, , description_type) VALUES (46, 'Flygande Jacob', 'https://www.ica.se/recept/flygande-jacob-717569/', 'flygande_jacob.jpg', 58);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (37, 'Laxsida i ugn', 'https://zeinaskitchen.se/hel-laxsida-i-ugn/', 34);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (45, 'Krämig svampkyckling', 'https://www.saltandlavender.com/creamy-mushroom-chicken/?fbclid=IwAR3o6VgU57vmQDUYdbrNOk-cYlYy0gNp_9mHPJCEdgKQwWdKPTwoiP4WmSo', 31);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (11, 'Karin hittar på något nytt', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 28);
+INSERT INTO public.recipes (id, name, url, image, queue_pos, description_type) VALUES (32, 'Vegetarisk lasagne', 'https://www.tasteline.com/recept/vegetarisk-lasagne-med-spenat-och-keso/', 'veg_lasagne.jpg', 2);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (24, 'Köttgryta', 'https://www.arla.se/recept/bygardens-kottgryta/?fbclid=IwAR3GeiH6b96FLRpUs8prXps7C7y2Za0BotG4rieKPqOtSbXagAW41WN_7N4', 10);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (26, 'Halloumipasta', 'https://www.ica.se/recept/kramig-pasta-med-halloumi-och-tomat-719023/?fbclid=IwAR1xArtwvt6rCF9gp3FWrgJiwSsQiEPIx_fEH2Ccvt2sqVCCO1erqeJcxT0', 6);
+INSERT INTO public.recipes (id, name, url, image, queue_pos, description_type) VALUES (14, 'Korv Stroganoff med ris', 'https://www.ica.se/recept/korvstroganoff-med-ris-533512/', 'korv_stroganoff.jpg', 1);
+INSERT INTO public.recipes (id, name, url, image, queue_pos, description_type) VALUES (36, 'Gulasch', 'https://www.tasteline.com/recept/gulaschsoppa-2/', 'gulasch.jpg', 4);
+INSERT INTO public.recipes (id, name, url, queue_pos, description_type) VALUES (41, 'Köttfärssoppa', 'https://www.ica.se/recept/kottfarssoppa-587342/?fbclid=IwAR1qP4O4PhFaAYfJVEMTSIcj_iPnzrI5U1w39XHlA3bP0UmynbuaGhRKLYY', 3);
+INSERT INTO public.recipes (id, name, url, image, queue_pos, description_type) VALUES (35, 'Falukorv i ugn', 'https://www.koket.se/morberg-lagar-husmanskost/per-morberg/falukorv-i-ugn-a-la-per-morberg/', 'falukorv_i_ugn.jpg', 0);
+INSERT INTO public.recipes (name, url, image) VALUES ('Club sandwichtårta', 'https://www.ica.se/recept/smorgastarta-a-la-club-sandwich-714805/', 'club_sandwich_cake.jpg');
+
+INSERT INTO public.recipes (id, name, queue_pos, description) VALUES (15, 'Ädelostgryta med fläsk', 9, 'Skär fläskfilén i bitar och bryn i en stekpanna eller gryta.
+Tillsätt allt utan paprika, låt koka ca 10 min.
+Smaka av med salt och peppar, tillsätt paprika och låt koka 5 min till.
+Servera med ris (gärna kokt).');
+
+INSERT INTO public.recipes (id, name, queue_pos, description , url) VALUES (43, 'Tortellinis med röd pestosås', 44, 'Koka tortellinis.
 Dela tomaterna i lagom bitar.
 Hiva i allt utan tortellinisarna i en kastrull, blanda och värm.
 Smaka av med salt och peppar, servera tortellinis och sås separat eller blandad.
 
 Hiva i stekt haloumi i mixen om du känner dig lite extra spexig.
 Det passar även bra med kokt broccoli som tillbehör.');
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (29, 'Paneer masala', 'https://www.arla.se/recept/paneer-masala/', 39, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (19, 'Pestopasta', '', 20, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (33, 'Köttbullar med mos', '/no_recipe_added/', 19, NULL);
-INSERT INTO public.recipes (id, name, url, image, queue_pos, description) VALUES (31, 'Biff Stroganoff', 'https://www.ica.se/recept/biff-stroganoff-713113/', 'biff_stroganoff.jpg', 14, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (34, 'Potatis- och purjulöksoppa', 'https://www.ica.se/recept/kramig-potatis-och-purjolokssoppa-716077/?fbclid=IwAR1iAeq9i_hTThumFcRYaDEQAlabl7McopIaJaC6qy3b-3BQGSTe8k0Jl1s', 8, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (38, 'Abbalax', 'https://www.abba.se/products/citronsas/', 24, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (22, 'Tomatsoppa', 'https://www.koket.se/anders_leven/soppor_och_grytor/gronsaker/tomatsoppa_med_basilikatoast/?fbclid=IwAR2pWyTsjepaonxuk0UIfIugu5YBtX-dN2BA7gYeTw1yLnZyFQ7b7hWcFsI', 64, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (17, 'Kräftstjärtspasta', 'https://www.arla.se/recept/morotspasta-med-kraftor/', 51, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (23, 'Tacopaj', '', 49, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (16, 'Bacon- och grönsaksgryta', '', 47, 'Stek bacon, champinjoner och lök.
+
+INSERT INTO public.recipes (id, name, queue_pos, description) VALUES (16, 'Bacon- och grönsaksgryta', 47, 'Stek bacon, champinjoner och lök.
 Tillsätt all creme fraiche och låt sjuda i ca 10 min.
 Hiva i paprikan och låt sjuda ca 5 min till.
 Servera med pasta.');
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (27, 'Enchiladas', 'https://www.santamariaworld.com/se/recept/enchiladas-med-kyckling/?fbclid=IwAR0xpPIemDLKOkArF5kHfIfT5aZYP53OgWB_UaAM5FPPBCljmAQ2gAsoYTA', 35, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (39, 'Fiskpinnar med stuvad spenat', '', 16, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (20, 'Krämig kycklinggryta med bacon och soltorkade tomater', 'https://www.tasteline.com/recept/kramig-kycklinggryta-med-kycklingbacon-och-soltorkade-tomater/', 13, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (21, 'Thaisoppa', '', 11, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (28, 'Asiatisk kokosgryta med lax', 'https://www.tasteline.com/recept/asiatisk-kokosgryta-med-lax/', 7, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (13, 'Busenkel korvröra', 'http://matochmera.se/recepie.php?recepie_id=31348', 104, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (12, 'Fredrik hittar på något nytt', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 98, NULL);
-INSERT INTO public.recipes (id, name, url, image, queue_pos, description) VALUES (46, 'Flygande Jacob', 'https://www.ica.se/recept/flygande-jacob-717569/', 'flygande_jacob.jpg', 58, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (37, 'Laxsida i ugn', 'https://zeinaskitchen.se/hel-laxsida-i-ugn/', 34, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (45, 'Krämig svampkyckling', 'https://www.saltandlavender.com/creamy-mushroom-chicken/?fbclid=IwAR3o6VgU57vmQDUYdbrNOk-cYlYy0gNp_9mHPJCEdgKQwWdKPTwoiP4WmSo', 31, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (11, 'Karin hittar på något nytt', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', 28, NULL);
-INSERT INTO public.recipes (id, name, url, image, queue_pos, description) VALUES (32, 'Vegetarisk lasagne', 'https://www.tasteline.com/recept/vegetarisk-lasagne-med-spenat-och-keso/', 'veg_lasagne.jpg', 2, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (24, 'Köttgryta', 'https://www.arla.se/recept/bygardens-kottgryta/?fbclid=IwAR3GeiH6b96FLRpUs8prXps7C7y2Za0BotG4rieKPqOtSbXagAW41WN_7N4', 10, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (15, 'Ädelostgryta med fläsk', '', 9, 'Skär fläskfilén i bitar och bryn i en stekpanna eller gryta.
-Tillsätt allt utan paprika, låt koka ca 10 min.
-Smaka av med salt och peppar, tillsätt paprika och låt koka 5 min till.
-Servera med ris (gärna kokt).');
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (26, 'Halloumipasta', 'https://www.ica.se/recept/kramig-pasta-med-halloumi-och-tomat-719023/?fbclid=IwAR1xArtwvt6rCF9gp3FWrgJiwSsQiEPIx_fEH2Ccvt2sqVCCO1erqeJcxT0', 6, NULL);
-INSERT INTO public.recipes (id, name, url, image, queue_pos, description) VALUES (14, 'Korv Stroganoff med ris', 'https://www.ica.se/recept/korvstroganoff-med-ris-533512/', 'korv_stroganoff.jpg', 1, NULL);
-INSERT INTO public.recipes (id, name, url, image, queue_pos, description) VALUES (36, 'Gulasch', 'https://www.tasteline.com/recept/gulaschsoppa-2/', 'gulasch.jpg', 4, NULL);
-INSERT INTO public.recipes (id, name, url, queue_pos, description) VALUES (41, 'Köttfärssoppa', 'https://www.ica.se/recept/kottfarssoppa-587342/?fbclid=IwAR1qP4O4PhFaAYfJVEMTSIcj_iPnzrI5U1w39XHlA3bP0UmynbuaGhRKLYY', 3, NULL);
-INSERT INTO public.recipes (id, name, url, image, queue_pos, description) VALUES (35, 'Falukorv i ugn', 'https://www.koket.se/morberg-lagar-husmanskost/per-morberg/falukorv-i-ugn-a-la-per-morberg/', 'falukorv_i_ugn.jpg', 0, NULL);
-INSERT INTO public.recipes (name, url, image) VALUES ('Club sandwichtårta', 'https://www.ica.se/recept/smorgastarta-a-la-club-sandwich-714805/', 'club_sandwich_cake.jpg');
-
 
 
 -- Data for Name: recipe_ingredients; Type: TABLE DATA; Schema: public; Owner: freppan
